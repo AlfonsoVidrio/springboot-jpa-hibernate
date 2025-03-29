04630139c5727c547792e7252b7f94bd8fcb5c22
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2025-03-28 22:48:46 -0600

    feat: add custom queries to find persons by id and name

--- src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa/repopsitories/PersonRepository.java
+++ src/main/java/com/alfonsovidrio/springboot/jpa/springboot_jpa/repopsitories/PersonRepository.java
@@ -1,6 +1,7 @@
 package com.alfonsovidrio.springboot.jpa.springboot_jpa.repopsitories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -13,9 +14,20 @@ public interface PersonRepository extends CrudRepository<Person, Long>{
     
     @Query("SELECT p FROM Person p WHERE p.programmingLanguage = ?1") 
     List<Person> findByProgrammingLanguage(String programmingLanguage);
-
+    
     List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
-
+    
     @Query("SELECT p.name, p.programmingLanguage FROM Person p")
     List<Object[]> findDataPerson();
+    
+    @Query("SELECT p FROM Person p WHERE p.id=?1")
+    Optional<Person> findOne(Long id);
+
+    @Query("SELECT p FROM Person p WHERE p.name=?1")
+    Optional<Person> findOneName(String name);
+
+    @Query("SELECT p FROM Person p WHERE p.name LIKE %?1%")
+    Optional<Person> findOneLikeName(String name);
+
+    Optional<Person> findByNameContaining(String name);
 }
